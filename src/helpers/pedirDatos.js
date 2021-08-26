5c0012986d55c07f50c899e2ff797d3bf1ef5bd7
ierluna <lunjavag@gmail.com>
2021-08-26 18:51:02 -0300

    se agrega comentario a pedirDatos.js

--- src/helpers/pedirDatos.js
+++ src/helpers/pedirDatos.js
@@ -1,7 +1,8 @@
 import { stock } from "../data/stock"
 
 export const pedirDatos = () => {
-
+    
+    //Delay de 2 segundos para simular retrasos de red
     return new Promise((resolve, reject) => {
 
             setTimeout(()=>{
